4fed6accb0c0d7e5f08140b9b4828e6227adf866
Mohammed Salama <mohamadsalama102@outlook.com>
2026-09-03 04:45:52 +0300

    set env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://localhost:7026/api',
-    fileUrl: 'https://localhost:7026/files'
- // apiUrl: 'https://api.hayatalaman.com/api'
+  //  apiUrl: 'https://localhost:7026/api',
+  //  fileUrl: 'https://localhost:7026/files'
+  apiUrl: 'https://api.hayatalaman.com/api',
+     fileUrl: 'https://localhost:7026/files'
 };
